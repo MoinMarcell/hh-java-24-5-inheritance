366e38b7efd0ef60f78738451ae333ccc68b4a82
Marcell <marcell.dechant@neuefische.de>
2024-11-27 10:35:14 +0100

    BIG BANG

---
+++ src/main/java/de/neuefische/Motorcycle.java
@@ -0,0 +1,11 @@
+package de.neuefische;
+
+import java.util.Objects;
+
+public class Motorcycle extends Vehicle {
+
+	public Motorcycle(String brand, String model, int horsePower) {
+		super(brand, model, horsePower);
+	}
+
+}
